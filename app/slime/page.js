a3835889d7f32f9e48c331f328c042c50cc483b2
side-devx <junprianto.girsang@kawanlamacorp.com>
2025-08-06 15:11:14 +0900

    Update page.js

--- app/slime/page.js
+++ app/slime/page.js
@@ -1,1 +1,10 @@
+"use client";
+
+export default function Placeholder() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-200">
+      <h1 className="text-2xl font-bold">Game Coming Soon</h1>
+    </div>
+  );
+}
 
